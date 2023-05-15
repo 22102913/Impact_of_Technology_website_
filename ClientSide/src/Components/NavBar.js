297f11354afbdd18ed7a898202b57b443bf4c230
22102913 <220993@student.wqe.ac.uk>
2023-05-15 21:55:33 +0100

    Fixed the nav bar

--- ClientSide/src/Components/NavBar.js
+++ ClientSide/src/Components/NavBar.js
@@ -1,4 +1,4 @@
-var underline = document.querySelector('#nav-underline');
+
 var item = document.querySelectorAll('a');
 var nav = document.querySelectorAll('#navbar-container');
 
@@ -11,27 +11,33 @@ export default function NavBar() {
     item.forEach(link => {
         link.addEventListener('mouseover', (e) => { SetUnderlinePos(e.target) })
         link.addEventListener('mouseout', (e) => {
-            underline.style.left = "0px";
-            underline.style.width = "100%";
+            
         })
+        
     })     
         
 
     return <nav id="nav">
 
-        <ul id="navbar-container">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about-us">About us</a></li>
-            <li><a href="/computers-in-the-work-force">Computers in the work force</a></li>
-            <li><a href="/about-us">About us</a></li>
+        <ul id="navbar-container" onMouseLeave={(e) => {ReSetunderlinePos(e) } }>
+            <li><a href="/" onMouseEnter={(e) => { SetUnderlinePos(e.target) }}>Home</a></li>
+            <li><a href="/about-us" onMouseEnter={(e) => { SetUnderlinePos(e.target) }}>About us</a></li>
+            <li><a href="/computers-in-the-work-force" onMouseEnter={(e) => { SetUnderlinePos(e.target) }}>Computers in the work force</a></li>
+            <li><a href="/about-us" onMouseEnter={(e) => { SetUnderlinePos(e.target) }}>About us</a></li>
         </ul>
         <div id="nav-underline"/>
     </nav>
 }
 
 function SetUnderlinePos(e) {
+    var underline = document.querySelector('#nav-underline');
     underline.style.left = e.offsetLeft + "px";
     document.cookie = e.offsetLeft + "px";
     underline.style.width = e.offsetWidth + "px";
+}
+function ReSetunderlinePos() {
+    var underline = document.querySelector('#nav-underline');
+    underline.style.left = "0px";
+    underline.style.width = "100%";console.log("-");
 }
 
