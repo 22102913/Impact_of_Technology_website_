254299d51d3856ff287c2e37079cb2cf74c1d640
22102913 <220993@student.wqe.ac.uk>
2023-05-19 13:56:12 +0100

    Created UserModel

--- ClientSide/src/pages/AboutUs.js
+++ ClientSide/src/pages/AboutUs.js
@@ -20,12 +20,6 @@ const AboutUs = () => {
             <p class="p0">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam. Vel facilisis volutpat est velit egestas dui id. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Mi sit amet mauris commodo. Cras adipiscing enim eu turpis. Vel eros donec ac odio tempor orci dapibus ultrices. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Vel pharetra vel turpis nunc eget lorem. Est ultricies integer quis auctor elit sed vulputate. Nisl rhoncus mattis rhoncus urna neque viverra justo nec.
             </p>
-            <p class="p2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam. Vel facilisis volutpat est velit egestas dui id. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Mi sit amet mauris commodo. Cras adipiscing enim eu turpis. Vel eros donec ac odio tempor orci dapibus ultrices. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Vel pharetra vel turpis nunc eget lorem. Est ultricies integer quis auctor elit sed vulputate. Nisl rhoncus mattis rhoncus urna neque viverra justo nec.
-            </p>
-            <p class="p1">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam. Vel facilisis volutpat est velit egestas dui id. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Mi sit amet mauris commodo. Cras adipiscing enim eu turpis. Vel eros donec ac odio tempor orci dapibus ultrices. Placerat orci nulla pellentesque dignissim enim sit amet venenatis urna. Vel pharetra vel turpis nunc eget lorem. Est ultricies integer quis auctor elit sed vulputate. Nisl rhoncus mattis rhoncus urna neque viverra justo nec.
-            </p>
         </div>
     );
 };
---
+++ ServerSide/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ServerSide.Models
+{
+    public class UserModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
