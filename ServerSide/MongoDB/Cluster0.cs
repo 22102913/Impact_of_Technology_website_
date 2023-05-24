0344f0b0659acedcc3ebed9e4227950a0b818f5b
22102913 <220993@student.wqe.ac.uk>
2023-05-24 10:35:42 +0100

    Changed Css for aboutPage and html bg

--- ServerSide/MongoDB/Cluster0.cs
+++ ServerSide/MongoDB/Cluster0.cs
@@ -36,30 +36,6 @@ namespace ServerSide.MongoDB
 
 		}
 
-		public static void Connect()
-		{
-
-            //const string connectionUri = "mongodb+srv://220993:WUPW9upw7XQ4pxYx@cluster0.hgf2zap.mongodb.net/?retryWrites=true&w=majority";
-            const string connectionUri = "mongodb+srv://220993:YMWq96hzhYJSV7qB@cluster0.hgf2zap.mongodb.net/?retryWrites=true&w=majority";
-            var settings = MongoClientSettings.FromConnectionString(connectionUri);
-            // Set the ServerApi field of the settings object to Stable API version 1
-            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
-            // Create a new client and connect to the server
-            var client = new MongoClient(settings);
-            // Send a ping to confirm a successful connection
-            try
-            {
-                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-                Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-
-        }
-
-
             public static string AddUser(UserModel user)
 		{
 			var email = new BsonDocument
--- ServerSide/Program.cs
+++ ServerSide/Program.cs
@@ -1,7 +1,5 @@
 using ServerSide.MongoDB;
 
-Cluster0.Connect();
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
